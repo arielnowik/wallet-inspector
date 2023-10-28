2d06f2ac7d21a6ae8d60bf4a1428f5fd1ad5089e
Tomas Olivieri <t_olivieri@farmacity.com.ar>
2023-10-27 23:40:17 -0300

    LongLine v2

--- LongLine.c
+++ LongLine.c
@@ -8,26 +8,35 @@ int main() {
     int h1;
     int contTotal = 0;
     int cont = 0;
+    int j;
+
 
     //Tomamos los valores ingresado por parametro
     scanf("%d %d %d %d %d", &n, &h0, &a, &c, &q);
 
-    //Iteramos hasta completar la fila
-    for (int i = 0; i < n; i++)
+    j = h0;
+
+    //Iteramos hasta completar la fila o cuando rompamos una restriccion
+    for (int i = 0; i < n - 1; i++)
     {
         h1 = ((a * h0) + c) % q;
+
+
         if (h1 > h0)
         {
+            if (j < h1)
+            {
+                j = h1;
+            }
             contTotal++;
-            contTotal += cont;
+            cont = 0;
         } else
         {
             cont++;
+            contTotal += cont;
         }
         h0 = h1;
     }
-
-    contTotal += cont;
 
     printf("%d", contTotal);
     return 0;
